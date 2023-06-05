cc0e96a5e2a715b9ac8ed9cacc9866ad8de46726
anatmb <anatmb26@hotmail.com>
2023-06-05 19:57:23 -0300

    first commit

---
+++ 01-IntroToCS/homework/homework.js
@@ -0,0 +1,12 @@
+"use strict";
+
+function BinarioADecimal(num) {
+  // escriben aca el codigo
+}
+
+function DecimalABinario(num) {}
+
+module.exports = {
+  BinarioADecimal,
+  DecimalABinario,
+};
